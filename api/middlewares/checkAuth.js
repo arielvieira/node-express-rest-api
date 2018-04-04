2cc8631a585fc39da8754ea0ad2a68c243d37fc0
Ariel Vieira <arielvieirasv@gmail.com>
2018-04-04 08:40:10 -0300

    Add configuration and env to the project

--- api/middlewares/checkAuth.js
+++ api/middlewares/checkAuth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const checkAuth = (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        const decoded = jwt.verify(token, '123');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.userData = decoded;
         next();
     } catch (error) {
